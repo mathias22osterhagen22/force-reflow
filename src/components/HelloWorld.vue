a3fd5f7d04299357d81b35072947486db8d1bfb5
mathias22osterhagen22 <mathias.osterhagen@protonmail.com>
2023-05-23 17:45:22 +0200

    feat: Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+function getNewA() {
+  let a = [];
+  for (let i = 0; i < 25000; i++) {
+    a.push({ id: i, value: Math.random() });
+  }
+  return a;
+}
+
+let myarray = ref(getNewA());
+setInterval(() => {
+  myarray.value = getNewA();
+}, 1000);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <table class="w-full">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item of myarray">
+        <td>{{ item.id }}</td>
+        <td>{{ item.value }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
